924dcdf9bb63458505c6af3971d8c93519d1b011
Iam-samyog <msamyog37@gmail.com>
2025-02-18 18:16:28 +0545

    Done all the things for the navbar responsive

--- scripts.js
+++ scripts.js
@@ -1,3 +1,7 @@
 document.getElementById("menuBtn").addEventListener("click", function() {
-    document.getElementById("mobileMenu").classList.toggle("hidden");
+    document.getElementById("mobileMenu").classList.toggle("show");
 });
+document.getElementById('mobileSearchBtn').addEventListener('click', function() {
+    const mobileSearchBar = document.getElementById('mobileSearchBar');
+    mobileSearchBar.classList.toggle('hidden');
+  });
